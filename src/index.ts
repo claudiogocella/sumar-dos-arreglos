5908c0af734fa45ba01c46541372e47ca4763a32
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-05-27 21:14:14 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+let dimensioArreglo: number = Number(prompt("Ingrese la dimrnsion del arreglo"));
+let numero1: number=Number(prompt("Ingrese numero maximo..."))
+let numero2: number=Number(prompt("Ingrese numero maximo..."))
+
+let primerArreglo : number[] = new Array(dimension);
+let segundoArreglo : number[] = new Array(dimension);
+let tercerArreglo : number[] = new Array(dimension);
+let indice ; number;
+
+//let cargarArreglo = (arreglo1: number[], arreglo2:number[], cantidad: number) {
+let cargarArreglo = (arreglo: number[], cantidad:number[], min: number, max:number):number[] {
+
+
+  for(indice=0; indice < cantidad ; indice++){
+
+    arreglo[indice] = crearAleatorio(min,max); 
+  
+  }
+return arreglo;
+//let cargarArreglo = (arreglo1: number[], arreglo2:number[], cantidad: number) {
+let sumar arreglos = (arreglo1:number[], arreglo2[], arreglo3:number[], cantidad:number) => {
+
+
+  }
+}
